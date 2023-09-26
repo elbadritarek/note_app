ac74aac48ae80b3e41d2a2a1c0d07cbc4b30f15b
elbadritarek <elbadritarek612@gmail.com>
2023-09-27 00:25:04 +0200

    edit note view ui done

--- lib/widgets/Edtting_Note_View_Body.dart
+++ lib/widgets/Edtting_Note_View_Body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/coustom_Appbar.dart';
+import 'package:notes_app/widgets/coustom_text_field.dart';
 
 class EdttingNoteViewBody extends StatelessWidget {
   const EdttingNoteViewBody({super.key});
@@ -7,11 +8,22 @@ class EdttingNoteViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
       child: Column(
         children: [
-          SizedBox(height: 22),
+          SizedBox(height: 50),
           coustomAppBar(icon: Icons.check, title: "Edti Note"),
+          SizedBox(
+            height: 50,
+          ),
+          coustomTextFeild(hintText: "Title"),
+          SizedBox(
+            height: 16,
+          ),
+          coustomTextFeild(
+            hintText: "Contant",
+            maxLines: 5,
+          )
         ],
       ),
     );
--- lib/widgets/Note_view_body.dart
+++ lib/widgets/Note_view_body.dart
@@ -8,10 +8,10 @@ class NoteViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      padding: EdgeInsets.symmetric(horizontal: 24.0),
       child: Column(
         children: [
-          SizedBox(height: 22),
+          SizedBox(height: 50),
           coustomAppBar(icon: Icons.search, title: "Note"),
           Expanded(child: NoteListView()),
         ],
